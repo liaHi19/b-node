e6a5a227fd5325e3136f577592e5a97a10aa5d1f
liaHi19 <gn.natalia19@gmail.com>
2022-12-12 17:51:33 +0200

    [#3] Add routes, install npm scripts, add nodemon

--- app.js
+++ app.js
@@ -1,39 +1,8 @@
 const http = require("http");
 const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const { url, method } = req;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Submit</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
+const { requestHandler } = require("./routes");
 
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  res.setHeader("Content-Type", "text-html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from Node.js server</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(requestHandler);
 
 server.listen(3001);
